251ca3d2bc30254ab13910f90deb6e604b408d72
Janaki <u16006110@tuks.co.za>
2019-03-12 17:32:42 +0200

    Added otp function

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express');
 conts app = express();
 
 //start 
+
+//generate otp pin, 5 digit long
+function generateOtp() 
+{
+  return Math.floor(Math.random() * 100000);
+}
