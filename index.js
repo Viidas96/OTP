15f701d09487a9741d874df58f31e4d96d29b839
Viidas96 <u16217498@tuks.co.za>
2019-03-13 15:17:37 +0200

    Added flatfile logging with a lock to prevent async function calls

--- index.js
+++ index.js
@@ -12,12 +12,9 @@ const frontEndUrl = "";
 const notificationUrl = "";
 
 //our endpoint and it expects a json object {'clientID':'someID'}
-//1. Require OTP so call this function from AUTH sub system
-app.post("/otp", async (req, res) => {
+app.post("/otp", async(req,res) => {
   //generate a 5 digit OTP
   let OTP = main.generateOtp();
-  console.log("Generated OTP:" + OTP);
-
   //timer with default 60 seconds
   let timer = new main.OTPToken();
 
@@ -28,16 +25,18 @@ app.post("/otp", async (req, res) => {
     headers: {'Content-Type': 'application/json'}
   })*/
   
+  alert(OTP);
+
   //a post to the ATM interface, userEnter will be the OTP the user entered in json form
-  /*const userEnter = await fetch(frontEndUrl, {
+  const userEnter = await fetch(frontEndUrl, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: {'Content-Type': 'application/json'}
-  });*/
+  });
 
   //compare OTP with userEnter, and check that timer.hasExpired is false then set valid to true and return the body to Authenticatrion
 
-  res.json(true);
+  res.json({valid});
 });
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -46,26 +45,7 @@ app.post("/otp", async (req, res) => {
   we need a get request that will respond with the 
 */
 
-//test "main" to demonstrate the working functionallity of the OTP 
-/*async function test(seconds) {
-  console.log('Creating token with 5 seconds expire time.');
-  let t = new OTPToken(5);
-  console.log('Token Created.');
-  console.log('Sleeping for ' + seconds + ' seconds');
-  await sleep(seconds * 1000);
-  console.log('Awaken');
-  console.log('Has your token expired?');
-  
-  if (t.hasExpired()) {
-    console.log('Yes');
-  } 
-  else { 
-    console.log('No');  
-  }
-}*/
-
-//main function calls
-//test(6)
-
 //end of OTPToken Test
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+main.insertFlatFile('1','55500' , '',true);
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const AsyncLock = require('async-lock');
+var lock = new AsyncLock();
 
 //const for flatfile storage
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -10,9 +12,72 @@ const flatFileString = (clientID, otp, successfull) => {
     }`;
 }
 
+//@author Bryan Janse Van Vuuren u16217498
+//This function will create and insert into a flatfile.
+function insertFlatFile(clientID, OTP, timestamp, success) {
+  let fileName = 'flatfile.json';
+  lock.acquire('key', function(done) {
+    // async work
+  var jsonContent = [];
+  fs.exists(fileName, function (exists) {
+    if (exists) {
+      var contents = fs.readFileSync(fileName);
+      if(contents.length == 0)
+      {
+        //console.log("No data in file");
+      }
+      else
+      {
+        jsonContent = JSON.parse(contents);
+      }
+      
+      let lastIndex = 0;
+      for (var i = 0; i < jsonContent.length; i++) {
+        var obj = jsonContent[i];
+        lastIndex = obj.id;
+      }
+      let insertLog = {
+        id: lastIndex + 1,
+        clientID: clientID,
+        OTP: OTP,
+        timestamp: timestamp,
+        success: success
+      };
+      jsonContent.push(insertLog);
+      fs.writeFile(fileName, JSON.stringify(jsonContent), function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log('Inserted into flatfile');
+      });
+    } else {
+      let insertLog = {
+        id: 0,
+        clientID: clientID,
+        OTP: OTP,
+        timestamp: timestamp,
+        success: success
+      };
+
+      let lines = [];
+      lines.push(insertLog);
+      fs.appendFile(fileName, JSON.stringify(lines), function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log('Inserted into flatfile');
+      });
+    }
+  });
+    
+}, function(err, ret) {
+    // lock released
+});
+  
+}
+
 //generate otp pin, 5 digit long
-function generateOtp() 
-{
+function generateOtp() {
   return Math.floor(Math.random() * 100000);
 }
 
@@ -23,27 +88,28 @@ function sleep(ms) {
 
 //The Token class
 class OTPToken {
-    //constructor for token
-    constructor(seconds = 60) {
-      this.resetToken(seconds)
-    }
-    
-    //set the valid time (the time the token is valid for)
-    resetToken(seconds = 60) {
-      this.currentTime = new Date();
-      this.expireTime = new Date(this.currentTime.getTime() + seconds * 1000);
-    }
-  
-    //returns whether the token has expired
-    hasExpired() {
-      var now = new Date();
-      return (this.expireTime < now);
-    }
+  //constructor for token
+  constructor(seconds = 60) {
+    this.resetToken(seconds)
   }
 
+  //set the valid time (the time the token is valid for)
+  resetToken(seconds = 60) {
+    this.currentTime = new Date();
+    this.expireTime = new Date(this.currentTime.getTime() + seconds * 1000);
+  }
+
+  //returns whether the token has expired
+  hasExpired() {
+    var now = new Date();
+    return (this.expireTime < now);
+  }
+}
+
 module.exports = {
-	flatFileString, 
-	generateOtp, 
-	sleep,
-	OTPToken
+  flatFileString,
+  generateOtp,
+  sleep,
+  OTPToken,
+  insertFlatFile
 };
