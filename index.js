52a79b534e88f0793b98df961c2bf9e921f70ce4
Francois <venter109@gmail.com>
2019-03-13 18:23:32 +0200

    added posts

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const frontEndUrl = "";
 const notificationUrl = "";
 
 //our endpoint and it expects a json object {'clientID':'someID'}
-app.post("/otp", async(req,res) => {
+app.post("/genotp", async(req,res) => {
   //generate a 5 digit OTP
   let OTP = main.generateOtp();
   //timer with default 60 seconds
@@ -24,28 +24,53 @@ app.post("/otp", async(req,res) => {
     body: JSON.stringify(body),
     headers: {'Content-Type': 'application/json'}
   })*/
-  
-  alert(OTP);
-
-  //a post to the ATM interface, userEnter will be the OTP the user entered in json form
-  const userEnter = await fetch(frontEndUrl, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {'Content-Type': 'application/json'}
-  });
-
   //compare OTP with userEnter, and check that timer.hasExpired is false then set valid to true and return the body to Authenticatrion
 
   res.json({valid});
 });
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*
-  TODO
-  we need a get request that will respond with the 
-*/
+app.post("/validate", async(req,res) => {
+  //generate a 5 digit OTP
+  let OTP = main.generateOtp();
+  //timer with default 60 seconds
+  let timer = new main.OTPToken();
 
-//end of OTPToken Test
+  //send a post request to Notification containing the clientID and the OTP we generated
+  /*const notified = await fetch(notificationUrl, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {'Content-Type': 'application/json'}
+  })*/
+  //compare OTP with userEnter, and check that timer.hasExpired is false then set valid to true and return the body to Authenticatrion
+
+  res.json({valid});
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.post("/getlogs", async(req,res) => {
+  //generate a 5 digit OTP
+  let OTP = main.generateOtp();
+  //timer with default 60 seconds
+  let timer = new main.OTPToken();
+
+  //send a post request to Notification containing the clientID and the OTP we generated
+  /*const notified = await fetch(notificationUrl, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {'Content-Type': 'application/json'}
+  })*/
+  //compare OTP with userEnter, and check that timer.hasExpired is false then set valid to true and return the body to Authenticatrion
+
+  res.json({valid});
+});
+
+
+//running the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+
+//testing thing
 main.insertFlatFile('1','55500' , '',true);
