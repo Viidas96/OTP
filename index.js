e5ae740b272540c7ee13d974ce252a822ff69040
Francois <venter109@gmail.com>
2019-03-14 22:55:19 +0200

    made a root catch with a description
    open localhost with no call to test

--- index.js
+++ index.js
@@ -7,6 +7,66 @@ const port = 8080;
 let clients = [];
 app.use(express.json());
 
+const rootStr = `<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Ocean OTP root page</title>
+		<style>
+			table{
+				margin: auto;
+			}
+			table, tr, td {
+				border: 1px solid black;
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<div>
+			<table style="width:900px">
+				<tr><td colspan="7"><p>Endpoints Available</p></td></tr>
+				<tr>
+					<td><p>Endpoint</p></td>
+					<td><p>Description</p></td>
+					<td><p>Method</p></td>
+					<td><p>Parameter</p></td>
+					<td><p>Example Parameters</p></td>
+					<td><p>Response</p></td>
+					<td><p>Example Response</p></td>
+				</tr>
+				<tr>
+					<td><code>/genotp</code></td>
+					<td><code>Generates a otp code and sends it to notification module</code></td>
+					<td><code>POST</code></td>
+					<td><code>clientID: string</code></td>
+					<td><code>'clientID': 'c12345'</code></td>
+					<td><code>status: bool</code></td>
+					<td><code>'status': true</code></td>
+				</tr>
+				<tr>
+					<td><code>/validate</code></td>
+					<td><code>Tests a givin OTP and returns the status</code></td>
+					<td><code>POST</code></td>
+					<td><code>clientID: string, otp: string</code></td>
+					<td><code>'clientID': 'c12345', 'otp': '12345'</code></td>
+					<td><code>status: bool</code></td>
+					<td><code>'status': true</code></td>
+				</tr>
+				<tr>
+					<td><code>/getlogs</code></td>
+					<td><code>Returns a JSON string with all the log items withing the givin dates, selected by day</code></td>
+					<td><code>POST</code></td>
+					<td><code>startDate : string, endDate: string</code></td>
+					<td><code>'startDate': '2019-03-13T18:56:42.121Z', 'endDate': '2019-03-13T19:02:16.323Z'</code></td>
+					<td><code>a list containing multiple objects of {id: int, clientID: string, OTP: string, timeStamp: string, success: bool</code></td>
+					<td><code>'id' : 1, 'clientID': 'c12345', 'OTP': '12345', 'timeStamp': '2019-03-13T19:07:30.695Z', 'success': true</code></td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>`;
+
 //This is needed
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,6 +77,10 @@ app.use(function(req, res, next) {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //The URL endpoints that we wil use to make POST requests
 const notificationUrl = "";
+
+app.get("/", (req, res) => {
+  res.send(rootStr);
+});
 
 //our endpoint and it expects a json object {'clientID':'someID'}
 app.post("/genotp", async (req, res) => {
