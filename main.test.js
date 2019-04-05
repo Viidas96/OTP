139a1659e624ed7d690d2fada854fcac5def5be8
Francois <venter109@gmail.com>
2019-04-05 12:48:29 +0200

    added notification test

--- main.test.js
+++ main.test.js
@@ -1,4 +1,5 @@
 const main = require("./main.js");
+const fetch = require('node-fetch');
 
 test('OTP has 5 characters', () => {
 	expect(main.generateOtp().toString().length).toBe(5);
@@ -16,3 +17,31 @@ test('Validate returns true if insert into flatfile correct',()=>
 {
 	expect(main.insertFlatFile('1','555000',new Date(),false));
 });
+
+test('Validate that Notifications responds',async ()=>
+{
+	const notificationUrl = "https://fnbsim.southafricanorth.cloudapp.azure.com/otp";
+	const data = await fetch(notificationUrl, {
+		method: 'POST',
+		body: JSON.stringify({
+		  clientID: "7381",
+		  otp: "12345"
+		}),
+		headers: { 'Content-Type': 'application/json' }
+	  });
+	expect(data).toBeDefined();
+});
+
+// test('Validate that Notifications responds',async ()=>
+// {
+// 	const reportingUrl = "https://fnbreports-6455.nodechef.com/api";
+// 	const data = await fetch(reportingUrl, {
+//         method: 'POST',
+//         body: JSON.stringify({
+//           system: "OTPS",
+//           data: result
+//         }),
+// 		headers: {'Content-Type': 'application/json'}
+// 	});
+// 	expect(data).toBeDefined();
+// });
